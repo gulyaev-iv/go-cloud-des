a4a88d1a0ad4b915b4d521501e3cd3c9e0d15179
Ilya Gulyaev <ilya.gulyaev2016@gmail.com>
2026-04-04 23:27:44 +0300

    rand: add Rand with Uniform, Triangular and Exponential distribution

---
+++ codegen/rand/rand.go
@@ -0,0 +1,32 @@
+package rand
+
+import (
+	"math"
+	"math/rand/v2"
+)
+
+type Rand struct {
+	*rand.Rand
+}
+
+func New(src rand.Source) *Rand {
+	return &Rand{rand.New(src)}
+}
+
+func (r *Rand) Uniform(min, max float64) float64 {
+	return min + r.Float64()*(max-min)
+}
+
+func (r *Rand) Triangular(min, mod, max float64) float64 {
+	u := r.Float64()
+	fc := (mod - min) / (max - min)
+
+	if u < fc {
+		return min + math.Sqrt(u*(max-min)*(mod-min))
+	}
+	return max - math.Sqrt((1-u)*(max-min)*(max-mod))
+}
+
+func (r *Rand) Exponential(lambda float64) float64 {
+	return r.ExpFloat64() / lambda
+}
